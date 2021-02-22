aa593bc7db4379e746f75a44d8505b9d2e3b10a2
athanwalker <athanwalker@email.arizona.edu>
2021-02-22 13:43:03 -0700

    Cleaner reg look, password stars

--- frontend/src/registration/registration.js
+++ frontend/src/registration/registration.js
@@ -11,19 +11,19 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />
 function Registration(props) {
 	return (
 		<form>
-			<h2>Account Registration Form</h2>
+			<h1>Account Registration Form</h1>
 			<div id="accountInfo">
 				<label>
-				    <p>Email (for account, recieve meeting information):</p>
+				    Email (for account, recieve meeting information):
 				    <input type="text" name="email" />
 				</label>
-				<label id="password" class="col1">
-					<p>Password:</p>
-					<input type="text" name="password" />
+				<label id="password" className="col1">
+					Password:
+					<input type="password" name="password" />
 				</label>
-				<label id="re-password" class="col2">
-					<p>Enter Password Again:</p>
-					<input type="text" name="re-password" />
+				<label id="re-password" className="col2">
+					Enter Password Again:
+					<input type="password" name="re-password" />
 				</label>
 			</div>
 			<div id="checkbox">
@@ -34,23 +34,24 @@ function Registration(props) {
             	/>
 				I understand that parents need to sign a consent form after registration
 			</div>
-			<hr />
 			<div id="studentInformation">
-				<h4>Student Information</h4>
-				<label class="col1">
-				    <p>Name:</p>
+				<hr />
+				<h3>Student Information</h3>
+				<label className="col1">
+				    Name:
 				    <input type="text" name="sname" />
 				</label>
-				<label class="col2">
-					<p>Age:</p>
+				<label className="col2">
+					Age:
 					<input type="text" name="age" />
 				</label>
-				<label class="col1">
-					<p>Grade:</p>
+				<label className="col1">
+					Grade:
 					<input type="text" name="grade" />
 				</label>
-				<label class="col2">
-					<p>Program Level:</p>
+				<label className="col2">
+					Program Level:
+					<br />
 					<select>
 						<option value='juniorA'>Junior A</option>
 						<option value='juniorB'>Junior B</option>
@@ -60,17 +61,17 @@ function Registration(props) {
 			</div>
 			<div id="parentInformation">
 			<hr />
-				<h4>Parent Information</h4>
-				<label class="col1">
-				    <p>Name:</p>
+				<h3>Parent Information</h3>
+				<label className="col1">
+				    Name:
 				    <input type="text" name="pname" />
 				</label>
-				<label class="col2">
-					<p>Phone:</p>
+				<label className="col2">
+					Phone:
 					<input type="text" name="phone" />
 				</label>
-				<label class="col1">
-					<p>Parent Email:</p>
+				<label className="col1">
+					Parent Email:
 					<input type="text" name="pemail" />
 				</label>
 			</div>
