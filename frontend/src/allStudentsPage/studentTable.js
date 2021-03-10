bfabd2d76972c0787a1efc2c5fe5138c15bae4b9
athanwalker <athanwalker@email.arizona.edu>
2021-03-10 10:10:57 -0700

    Table page setup

--- frontend/src/allStudentsPage/studentTable.js
+++ frontend/src/allStudentsPage/studentTable.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 function StudentTable(props) {
-  const table = (
+  const startTable = (
     <table border="5">
       <tr>
         <th>Student Name</th>
@@ -12,17 +12,9 @@ function StudentTable(props) {
     </table>
   );
 
-  return (
-    <table border="5">
-      <tr>
-        <th>Student Name</th>
-        <th>Parent Name</th>
-        <th>Contact Phone</th>
-        <th>Consent Form</th>
-        <th> Verified </th>
-      </tr>
-    </table>
-  );
+  const [table, setTable] = useState({ startTable });
+
+  return { table };
 }
 
 export default StudentTable;
