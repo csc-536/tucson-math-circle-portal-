2bf7c193557f3b4cfd7732c29af5d7971072d3f2
Ryan Sullivant <rsulli55@gmail.com>
2021-03-11 16:50:41 -0700

    Added role to tokens, added functions to get student or admin (checking the role of the token), added update operations for the student

--- backend/auth/db/models/users.py
+++ backend/auth/db/models/users.py
@@ -4,10 +4,12 @@ from uuid import uuid4
 from pydantic import BaseModel, Field, EmailStr, UUID4, validator
 from enum import Enum
 
+
 class UserRole(str, Enum):
     student = "student"
     admin = "admin"
     coordinator = "coordinator"
+
 
 class User(BaseModel):
     id: UUID4 = Field(default_factory=uuid4)
@@ -25,11 +27,12 @@ class UserCreate(User):
             raise ValueError("Password should be at least 6 characters")
         return v
 
+
 class UserUpdate(BaseModel):
     email: Optional[EmailStr]
-    role: Optional[str]
     password: Optional[str]
     disabled: Optional[bool] = None
+
 
 class UserInDB(User):
     hashed_password: str
