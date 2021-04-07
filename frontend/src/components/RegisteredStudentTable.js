a98d741a98e620541ab0c9c9cfea158b94626ed2
jiachengzhang1 <jiachengzhang1@email.arizona.edu>
2021-04-07 09:16:23 -0700

    run lint

--- frontend/src/components/RegisteredStudentTable.js
+++ frontend/src/components/RegisteredStudentTable.js
@@ -1,110 +1,95 @@
 import {
-    Checkbox,
-    makeStyles,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    withStyles,
+  Checkbox,
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  withStyles,
 } from "@material-ui/core";
 import { uniqueId } from "lodash";
 import React from "react";
 
 const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-        backgroundColor: "white",
-    },
-    container: {
-        maxHeight: 600,
-    },
+  table: {
+    minWidth: 650,
+    backgroundColor: "white",
+  },
+  container: {
+    maxHeight: 600,
+  },
 });
 
 const StyledTableCell = withStyles((theme) => ({
-    head: {
-        // backgroundColor: theme.palette.common.white,
-        fontWeight: 800,
-        fontSize: "16px",
-    },
+  head: {
+    // backgroundColor: theme.palette.common.white,
+    fontWeight: 800,
+    fontSize: "16px",
+  },
 }))(TableCell);
 
 const RegisteredStudentTable = ({ students, handleCheckAttended }) => {
-    const classes = useStyles();
-    console.log(students);
-    return (
-        <>
-            <h3>Registered Students</h3>
-            <TableContainer className={classes.container}>
-                <Table
-                    stickyHeader
-                    className={classes.table}
-                    aria-label="registered-student-table"
-                >
-                    <TableHead className={classes.head}>
-                        <TableRow className={classes.head}>
-                            <StyledTableCell>Student Name</StyledTableCell>
-                            <StyledTableCell align="right">
-                                Parent Name
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Contact Phone
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Additional Contacts
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Attended
-                            </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {students.map(
-                            (
-                                {
-                                    studentName,
-                                    parentName,
-                                    contactPhone,
-                                    additionalContacts,
-                                    attended,
-                                },
-                                i
-                            ) => (
-                                <TableRow key={uniqueId()}>
-                                    <TableCell component="th" scope="row">
-                                        {studentName}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        {parentName}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        {contactPhone}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        {additionalContacts}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <Checkbox
-                                            checked={attended}
-                                            onChange={(e) =>
-                                                handleCheckAttended(e, i)
-                                            }
-                                            color="primary"
-                                            inputProps={{
-                                                "aria-label":
-                                                    "secondary checkbox",
-                                            }}
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        )}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    );
+  const classes = useStyles();
+  console.log(students);
+  return (
+    <>
+      <h3>Registered Students</h3>
+      <TableContainer className={classes.container}>
+        <Table
+          stickyHeader
+          className={classes.table}
+          aria-label="registered-student-table"
+        >
+          <TableHead className={classes.head}>
+            <TableRow className={classes.head}>
+              <StyledTableCell>Student Name</StyledTableCell>
+              <StyledTableCell align="right">Parent Name</StyledTableCell>
+              <StyledTableCell align="right">Contact Phone</StyledTableCell>
+              <StyledTableCell align="right">
+                Additional Contacts
+              </StyledTableCell>
+              <StyledTableCell align="right">Attended</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map(
+              (
+                {
+                  studentName,
+                  parentName,
+                  contactPhone,
+                  additionalContacts,
+                  attended,
+                },
+                i
+              ) => (
+                <TableRow key={uniqueId()}>
+                  <TableCell component="th" scope="row">
+                    {studentName}
+                  </TableCell>
+                  <TableCell align="right">{parentName}</TableCell>
+                  <TableCell align="right">{contactPhone}</TableCell>
+                  <TableCell align="right">{additionalContacts}</TableCell>
+                  <TableCell align="right">
+                    <Checkbox
+                      checked={attended}
+                      onChange={(e) => handleCheckAttended(e, i)}
+                      color="primary"
+                      inputProps={{
+                        "aria-label": "secondary checkbox",
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default RegisteredStudentTable;
--- frontend/src/pages/MeetingInfo.js
+++ frontend/src/pages/MeetingInfo.js
@@ -9,177 +9,170 @@ import { useHistory, useLocation } from "react-router";
 import { deleteMeeting, updateMeeting } from "../http";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexWrap: "wrap",
-    },
-    button: {
-        marginTop: theme.spacing(3),
-        marginLeft: theme.spacing(3),
-        float: "right",
-    },
+  root: {
+    flexWrap: "wrap",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(3),
+    float: "right",
+  },
 }));
 
 const MeetingInfo = () => {
-    const classes = useStyles();
-    const location = useLocation();
-    const [disabled, setDisabled] = useState(false);
-    const history = useHistory();
-    const [form, setForm] = useState({
-        date: new Date(),
-        time: "",
-        duration: "",
-        topic: "",
-        sessionLevel: "",
-        material: "",
-        zoomLink: "",
-        zoomPassword: "",
-        miroLink: "",
-        notes: "",
-        students: [],
+  const classes = useStyles();
+  const location = useLocation();
+  const [disabled, setDisabled] = useState(false);
+  const history = useHistory();
+  const [form, setForm] = useState({
+    date: new Date(),
+    time: "",
+    duration: "",
+    topic: "",
+    sessionLevel: "",
+    material: "",
+    zoomLink: "",
+    zoomPassword: "",
+    miroLink: "",
+    notes: "",
+    students: [],
+  });
+
+  useEffect(() => {
+    const {
+      state: {
+        meeting: {
+          date,
+          sessionLevel,
+          zoom_link,
+          miro_link,
+          zoomPassword,
+          topic,
+          duration,
+          students,
+          uuid,
+        },
+        past,
+      },
+    } = location;
+    setForm({
+      ...form,
+      ...{
+        date,
+        time: getTime(date),
+        topic,
+        sessionLevel,
+        zoomLink: zoom_link,
+        miroLink: miro_link,
+        zoomPassword,
+        duration: getTimeDifferences(duration, date),
+        uuid,
+        students,
+      },
     });
+    setDisabled(past);
+  }, [location]);
 
-    useEffect(() => {
-        const {
-            state: {
-                meeting: {
-                    date,
-                    sessionLevel,
-                    zoom_link,
-                    miro_link,
-                    zoomPassword,
-                    topic,
-                    duration,
-                    students,
-                    uuid,
-                },
-                past,
-            },
-        } = location;
-        setForm({
-            ...form,
-            ...{
-                date,
-                time: getTime(date),
-                topic,
-                sessionLevel,
-                zoomLink: zoom_link,
-                miroLink: miro_link,
-                zoomPassword,
-                duration: getTimeDifferences(duration, date),
-                uuid,
-                students,
-            },
-        });
-        setDisabled(past);
-    }, [location]);
+  const handleCheckAttended = (e, i) => {
+    const students = cloneDeep(form["students"]);
+    students[i].attended = e.target.checked;
+    setForm({
+      ...form,
+      ...{ students },
+    });
+  };
 
-    const handleCheckAttended = (e, i) => {
-        const students = cloneDeep(form["students"]);
-        students[i].attended = e.target.checked;
-        setForm({
-            ...form,
-            ...{ students },
-        });
-    };
+  const handleSaveMeeting = async (e) => {
+    e.preventDefault();
+    const {
+      students,
+      date: date_and_time,
+      miroLink: miro_link,
+      sessionLevel: session_level,
+      material: materials_link,
+      zoomLink: zoom_link,
+      duration,
+      topic,
+      uuid: meeting_id,
+    } = form;
+    console.log(form);
 
-    const handleSaveMeeting = async (e) => {
-        e.preventDefault();
-        const {
-            students,
-            date: date_and_time,
-            miroLink: miro_link,
-            sessionLevel: session_level,
-            material: materials_link,
-            zoomLink: zoom_link,
-            duration,
-            topic,
-            uuid: meeting_id,
-        } = form;
-        console.log(form);
+    // TODO: Validate inputs
 
-        // TODO: Validate inputs
+    // TODO: submit form
+    try {
+      await updateMeeting({
+        date_and_time,
+        duration,
+        zoom_link,
+        session_level,
+        topic,
+        miro_link,
+        coordinator_notes: "ddd",
+        student_notes: "ddddddd",
+        materials_link: "https://test.com",
+        meeting_id,
+      });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
-        // TODO: submit form
-        try {
-            await updateMeeting({
-                date_and_time,
-                duration,
-                zoom_link,
-                session_level,
-                topic,
-                miro_link,
-                coordinator_notes: "ddd",
-                student_notes: "ddddddd",
-                materials_link: "https://test.com",
-                meeting_id,
-            });
-        } catch (error) {
-            console.log(error.response);
-        }
-    };
+  const handleDelete = async () => {
+    try {
+      console.log(form["uuid"]);
+      await deleteMeeting(form["uuid"]);
+      history.push("/meetings");
+      console.log("delete");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const handleDelete = async () => {
-        try {
-            console.log(form["uuid"]);
-            await deleteMeeting(form["uuid"]);
-            history.push("/meetings");
-            console.log("delete");
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <div>
-            <form
-                className={classes.root}
-                noValidate
-                autoComplete="off"
-                id="new-meeting-form"
-                onSubmit={handleSaveMeeting}
-            >
-                <MeetingFields
-                    form={form}
-                    setForm={setForm}
-                    disabled={disabled}
-                />
-                <RegisteredStudentTable
-                    students={form["students"]}
-                    handleCheckAttended={handleCheckAttended}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    startIcon={<SaveIcon />}
-                    type="submit"
-                    disableElevation
-                >
-                    Save
-                </Button>
-            </form>
-            <DeleteButton
-                deleteAction={handleDelete}
-                className={classes.button}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        id="new-meeting-form"
+        onSubmit={handleSaveMeeting}
+      >
+        <MeetingFields form={form} setForm={setForm} disabled={disabled} />
+        <RegisteredStudentTable
+          students={form["students"]}
+          handleCheckAttended={handleCheckAttended}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          startIcon={<SaveIcon />}
+          type="submit"
+          disableElevation
+        >
+          Save
+        </Button>
+      </form>
+      <DeleteButton deleteAction={handleDelete} className={classes.button} />
+    </div>
+  );
 };
 
 function getTimeDifferences(d1, d2) {
-    const diff = new Date(d1).getTime() - new Date(d2).getTime();
-    return diff / 1000 / 60;
+  const diff = new Date(d1).getTime() - new Date(d2).getTime();
+  return diff / 1000 / 60;
 }
 
 function getTime(d) {
-    const date = new Date(d);
-    const h = toTwoDigits(date.getHours());
-    const m = toTwoDigits(date.getMinutes());
-    return `${h}:${m}`;
+  const date = new Date(d);
+  const h = toTwoDigits(date.getHours());
+  const m = toTwoDigits(date.getMinutes());
+  return `${h}:${m}`;
 }
 
 function toTwoDigits(s) {
-    return s < 10 ? `0${s}` : `${s}`;
+  return s < 10 ? `0${s}` : `${s}`;
 }
 
 export default MeetingInfo;
