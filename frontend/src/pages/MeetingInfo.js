8b83288623f1bdfe9c5980036daf8bdfb0ae56be
jiachengzhang1 <jiachengzhang1@email.arizona.edu>
2021-04-07 09:15:58 -0700

    support update meeting

--- frontend/src/pages/MeetingInfo.js
+++ frontend/src/pages/MeetingInfo.js
@@ -5,167 +5,181 @@ import RegisteredStudentTable from "../components/RegisteredStudentTable";
 import SaveIcon from "@material-ui/icons/Save";
 import { cloneDeep } from "lodash";
 import DeleteButton from "../components/DeleteButton";
-import { useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router";
+import { deleteMeeting, updateMeeting } from "../http";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexWrap: "wrap",
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(3),
-    float: "right",
-  },
+    root: {
+        flexWrap: "wrap",
+    },
+    button: {
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(3),
+        float: "right",
+    },
 }));
 
 const MeetingInfo = () => {
-  const classes = useStyles();
-
-  const location = useLocation();
-
-  const [disabled, setDisabled] = useState(false);
-
-  const [form, setForm] = useState({
-    date: new Date(),
-    time: "",
-    duration: "",
-    topic: "",
-    sessionLevel: "",
-    material: "",
-    zoomLink: "",
-    zoomPassword: "",
-    miroLink: "",
-    notes: "",
-  });
-
-  useEffect(() => {
-    const {
-      state: {
-        meeting: {
-          date,
-          sessionLevel,
-          zoom_link,
-          miro_link,
-          zoomPassword,
-          topic,
-          duration,
-          students,
-        },
-        past,
-      },
-    } = location;
-    setForm({
-      ...form,
-      ...{
-        date,
-        time: getTime(date),
-        topic,
-        sessionLevel,
-        zoomLink: zoom_link,
-        miroLink: miro_link,
-        zoomPassword,
-        duration: getTimeDifferences(duration, date),
-      },
+    const classes = useStyles();
+    const location = useLocation();
+    const [disabled, setDisabled] = useState(false);
+    const history = useHistory();
+    const [form, setForm] = useState({
+        date: new Date(),
+        time: "",
+        duration: "",
+        topic: "",
+        sessionLevel: "",
+        material: "",
+        zoomLink: "",
+        zoomPassword: "",
+        miroLink: "",
+        notes: "",
+        students: [],
     });
-    // console.log(topic);
-    // setRegisteredStudents(students);
-    setDisabled(past);
-  }, [location]);
 
-  const [registeredStudents, setRegisteredStudents] = useState([
-    {
-      studentName: "Joe Doe1",
-      parentName: "Jane Doe",
-      contactPhone: 1234567890,
-      additionalContacts: 24,
-      attended: false,
-    },
-    {
-      studentName: "Joe Doe2",
-      parentName: "Jane Doe",
-      contactPhone: 1234567890,
-      additionalContacts: 24,
-      attended: false,
-    },
-    {
-      studentName: "Joe Doe3",
-      parentName: "Jane Doe",
-      contactPhone: 1234567890,
-      additionalContacts: 24,
-      attended: false,
-    },
-    {
-      studentName: "Joe Doe3",
-      parentName: "Jane Doe",
-      contactPhone: 1234567890,
-      additionalContacts: 24,
-      attended: false,
-    },
-  ]);
+    useEffect(() => {
+        const {
+            state: {
+                meeting: {
+                    date,
+                    sessionLevel,
+                    zoom_link,
+                    miro_link,
+                    zoomPassword,
+                    topic,
+                    duration,
+                    students,
+                    uuid,
+                },
+                past,
+            },
+        } = location;
+        setForm({
+            ...form,
+            ...{
+                date,
+                time: getTime(date),
+                topic,
+                sessionLevel,
+                zoomLink: zoom_link,
+                miroLink: miro_link,
+                zoomPassword,
+                duration: getTimeDifferences(duration, date),
+                uuid,
+                students,
+            },
+        });
+        setDisabled(past);
+    }, [location]);
 
-  const handleCheckAttended = (e, i) => {
-    const students = cloneDeep(registeredStudents);
-    students[i].attended = e.target.checked;
-    setRegisteredStudents(students);
-  };
+    const handleCheckAttended = (e, i) => {
+        const students = cloneDeep(form["students"]);
+        students[i].attended = e.target.checked;
+        setForm({
+            ...form,
+            ...{ students },
+        });
+    };
 
-  const handleSaveMeeting = (e) => {
-    e.preventDefault();
-    console.log(form);
+    const handleSaveMeeting = async (e) => {
+        e.preventDefault();
+        const {
+            students,
+            date: date_and_time,
+            miroLink: miro_link,
+            sessionLevel: session_level,
+            material: materials_link,
+            zoomLink: zoom_link,
+            duration,
+            topic,
+            uuid: meeting_id,
+        } = form;
+        console.log(form);
 
-    // TODO: Validate inputs
+        // TODO: Validate inputs
 
-    // TODO: submit form
-  };
+        // TODO: submit form
+        try {
+            await updateMeeting({
+                date_and_time,
+                duration,
+                zoom_link,
+                session_level,
+                topic,
+                miro_link,
+                coordinator_notes: "ddd",
+                student_notes: "ddddddd",
+                materials_link: "https://test.com",
+                meeting_id,
+            });
+        } catch (error) {
+            console.log(error.response);
+        }
+    };
 
-  const handleDelete = () => {
-    console.log("delete");
-  };
+    const handleDelete = async () => {
+        try {
+            console.log(form["uuid"]);
+            await deleteMeeting(form["uuid"]);
+            history.push("/meetings");
+            console.log("delete");
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  return (
-    <div>
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        id="new-meeting-form"
-        onSubmit={handleSaveMeeting}
-      >
-        <MeetingFields form={form} setForm={setForm} disabled={disabled} />
-        <RegisteredStudentTable
-          students={registeredStudents}
-          handleCheckAttended={handleCheckAttended}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          startIcon={<SaveIcon />}
-          type="submit"
-          disableElevation
-        >
-          Save
-        </Button>
-      </form>
-      <DeleteButton deleteAction={handleDelete} className={classes.button} />
-    </div>
-  );
+    return (
+        <div>
+            <form
+                className={classes.root}
+                noValidate
+                autoComplete="off"
+                id="new-meeting-form"
+                onSubmit={handleSaveMeeting}
+            >
+                <MeetingFields
+                    form={form}
+                    setForm={setForm}
+                    disabled={disabled}
+                />
+                <RegisteredStudentTable
+                    students={form["students"]}
+                    handleCheckAttended={handleCheckAttended}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    startIcon={<SaveIcon />}
+                    type="submit"
+                    disableElevation
+                >
+                    Save
+                </Button>
+            </form>
+            <DeleteButton
+                deleteAction={handleDelete}
+                className={classes.button}
+            />
+        </div>
+    );
 };
 
 function getTimeDifferences(d1, d2) {
-  const diff = new Date(d1).getTime() - new Date(d2).getTime();
-  return diff / 1000 / 60;
+    const diff = new Date(d1).getTime() - new Date(d2).getTime();
+    return diff / 1000 / 60;
 }
 
 function getTime(d) {
-  const date = new Date(d);
-  const h = toTwoDigits(date.getHours());
-  const m = toTwoDigits(date.getMinutes());
-  return `${h}:${m}`;
+    const date = new Date(d);
+    const h = toTwoDigits(date.getHours());
+    const m = toTwoDigits(date.getMinutes());
+    return `${h}:${m}`;
 }
 
 function toTwoDigits(s) {
-  return s < 10 ? `0${s}` : `${s}`;
+    return s < 10 ? `0${s}` : `${s}`;
 }
 
 export default MeetingInfo;
